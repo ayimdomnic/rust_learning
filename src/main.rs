e529dd41efaa5d8fecbf0d2e26f88d7314279100
Odhiambo Dormnic <ayimdomnic@outlook.com>
2022-05-02 12:04:14 +0300

    chore: demostration of collections

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,32 @@
-struct Boeing {
-    required_crew: u8,
-    range: u16,
-}
-
-struct Airbus {
-    required_crew: u8,
-    range: u16,
-}
-
-trait Flight {
-    fn is_ilegal(&self, required_crew: u8, available_crew: u8, range: u16, distance: u16) -> bool;
-}
-
-impl Flight for Boeing {
-    fn is_ilegal(&self, required_crew: u8, available_crew: u8, range: u16, distance: u16) -> bool {
-        if (available_crew >= required_crew) && (range + 150 >= distance) {
-            true
-        } else {
-            false
-        }
-    }
-}
-
-impl Flight for Airbus {
-    fn is_ilegal(&self, required_crew: u8, available_crew: u8, range: u16, distance: u16) -> bool {
-        if (available_crew >= required_crew) && (range + 280 >= distance) {
-            true
-        } else {
-            false
-        }
-    }
-}
+use std::collections::HashMap;
+//Arrays and tuples
+//Stored in stack memory
+//collection have multiple types
+//collections are stored on the heap,
+//so they are allocated on the heap
+//can grow and shrink
+//sequences, maps & sets
+//sequesnce is a collection of items you can add, remove, and iterate over
+//ordered lists
 
 fn main() {
-    let boeing = Boeing {
-        required_crew: 4,
-        range: 7370,
-    };
+    let mut flights = HashMap::new();
 
-    let airbus = Airbus {
-        required_crew: 7,
-        range: 5280,
-    };
+    flights.insert("DA918", ("11:45", "Orlando"));
+    flights.insert("DA919", ("12:05", "Salt lake city"));
+    flights.insert("DA920", ("12:25", "Denver"));
+    flights.insert("DA921", ("12:45", "Phoenix"));
+    flights.insert("DA922", ("13:05", "Los angeles"));
+    flights.insert("DA923", ("13:25", "San diego"));
 
-    let boeing_is_legal = boeing.is_ilegal(boeing.required_crew, 18, boeing.range, 2385);
-    let airbus_is_legal = airbus.is_ilegal(airbus.required_crew, 4, airbus.range, 2385);
+    let flight_number = "DA918";
+
+    let option = flights.get(flight_number);
+
+    let (time, destination) = option.unwrap();
 
     println!(
-        "Boeing is legal: {}\nAirbus is legal: {}",
-        boeing_is_legal, airbus_is_legal
+        "The flight {} is leaving at {} and will be going to {}",
+        flight_number, time, destination
     );
 }
-
-//methods
-//Traits
-// Traits are a way to share behavior between different types.
-//Language: rust
