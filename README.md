07ae262314ec918d628fc6e92cd88913516e407e
Odhiambo Dormnic <ayimdomnic@outlook.com>
2022-05-02 13:55:10 +0300

    chore: mastering cargo

---
+++ README.md
@@ -0,0 +1,1 @@
+### learning Rust language today
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 mod geo;
 use rand::Rng;
 
-//multiproducer singleconsumer - many channels can send messages to the channel only one can receive
-
 fn main() {
     let mut rng = rand::thread_rng();
     let random_number: f64 = rng.gen();
     println!("{}", random_number);
+    geo::distance(0.0, 0.0, 0.0, 0.0);
 }
 
 
