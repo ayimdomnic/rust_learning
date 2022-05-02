4dc0e88b580e9f8fb81b07a6da1553314c290490
Odhiambo Dormnic <ayimdomnic@outlook.com>
2022-05-02 12:41:39 +0300

    chore: generics are like typescript types

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,33 @@
-use std::collections::HashMap;
-use std::collections::HashSet;
-//Arrays and tuples
-//Stored in stack memory
-//collection have multiple types
-//collections are stored on the heap,
-//so they are allocated on the heap
-//can grow and shrink
-//sequences, maps & sets
-//sequesnce is a collection of items you can add, remove, and iterate over
-//ordered lists
+#[derive(Debug)]
+struct NavAid<T, U> {
+    name: String,
+    frequency: T,
+    data: U,
+}
 
 fn main() {
-    let mut flights = HashMap::new();
+   //declaring a variable as a generic
+   //using a constraint
+    let vor = NavAid {
+         name: String::from("DQN"),
+         frequency: 7.5,
+         data: String::from("VOR"),
+    };
 
-    flights.insert("DA918", ("11:45", "Orlando"));
-    flights.insert("DA919", ("12:05", "Salt lake city"));
-    flights.insert("DA920", ("12:25", "Denver"));
-    flights.insert("DA921", ("12:45", "Phoenix"));
-    flights.insert("DA922", ("13:05", "Los angeles"));
-    flights.insert("DA923", ("13:25", "San diego"));
+    let ndb_data: Option<String> = Option::None;
 
-    let flight_number = "DA918";
+    let ndb = NavAid {
+        name: String::from("DQN"),
+        frequency: 7.5,
+        data: ndb_data
+    };
 
-    let option = flights.get(flight_number);
+    println!("VOR information is: {:?}", vor);
+    println!("NDB information is: {:?}", ndb);
 
-    let (time, destination) = option.unwrap();
 
-    println!(
-        "The flight {} is leaving at {} and will be going to {}",
-        flight_number, time, destination
-    );
 
-    //contains_key returns true if the key is in the map
-    if !flights.contains_key(flight_number) {
-        println!("Flight {} is not in the database", flight_number);
-        flights.insert(flight_number, ("13:45", "New york"));
-    } else {
-        println!("Flight {} is in the database", flight_number);
-    }
 
-    let mut set_flights = HashSet::new();
 
-    set_flights.insert(("DA918", "11:45", "Orlando"));
-    set_flights.insert(("DA919", "12:05", "Salt lake city"));
-    set_flights.insert(("DA920", "12:25", "Denver"));
-    set_flights.insert(("DA921", "12:45", "Phoenix"));
-    set_flights.insert(("DA922", "13:05", "Los angeles"));
-    set_flights.insert(("DA923", "13:25", "San diego"));
-    //sets are collections of unique items
 
-    for item in set_flights.iter() {
-        println!("{:?}", item);
-    }
 }
